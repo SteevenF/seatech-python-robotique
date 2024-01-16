4b45b7f6e6d1ce5eee9f179aaf2d8969a2b2e7a6
SteevenF <31657065+SteevenF@users.noreply.github.com>
2024-01-16 14:33:35 +0100

    V1.1

--- exo2.py
+++ exo2.py
@@ -2,22 +2,24 @@ from time import sleep
 from exo1 import Robot
 
 class Human():
+    __sexe = None
     def __init__(self, sexe):
-        self.__sexe = sexe
-        self.__aliments_manges = []
-        self.__nbre_aliments_manges = 0
+        if sexe == 'M' or sexe == 'F' or sexe == 'H':
+            self.__sexe = sexe
+            self.__aliments_manges = []
+            self.__nbre_aliments_manges = 0
 
     def eat(self, aliments):
         if type(aliments) is str:
+            self.__aliments_manges.append(aliments)
             self.__nbre_aliments_manges += 1
             print("Je mange {} ({} aliments mangés)".format(aliments, self.__nbre_aliments_manges))
-        else:
+        elif type(aliments) is list and type(aliments[0]) is str:
+            self.__aliments_manges.extend(aliments)
             for aliment in aliments:
                 self.__nbre_aliments_manges += 1
                 print("Je mange {} ({} aliments mangés)".format(aliment, self.__nbre_aliments_manges))
         
-        self.__aliments_manges.append(aliments)
-
     def digest(self):
         for i in range(self.__nbre_aliments_manges):
             print("Digestion en cours ({}/{})".format(i+1, self.__nbre_aliments_manges), flush=True, end='\r')
@@ -29,7 +31,8 @@ class Human():
         return self.__sexe
     @sexe.setter
     def sexe(self, new_sexe):
-        self.__sexe = new_sexe
+        if new_sexe == 'M' or new_sexe == 'F' or new_sexe == 'H':
+            self.__sexe = new_sexe
 
 
 class Cyborg(Robot, Human):   
@@ -37,8 +40,8 @@ class Cyborg(Robot, Human):
         Robot.__init__(self, name)
         Human.__init__(self, sexe)
 
-    def truc_fun():
-        pass
+    def listen_people(self):
+        print('Conversation entendue à 2km :\n"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna eget est lorem ipsum dolor sit amet consectetur. Tortor condimentum lacinia quis vel eros donec ac. Auctor urna nunc id cursus metus aliquam eleifend mi. Parturient montes nascetur ridiculus mus mauris."')
 
 if __name__ == "__main__":
     cyborg = Cyborg('Deux Ex Machina', 'M')
@@ -50,4 +53,4 @@ if __name__ == "__main__":
     cyborg.eat('banana')
     cyborg.eat(['coca', 'chips'])
     cyborg.digest()
-    # cyborg.truc_fun()
+    cyborg.listen_people()
